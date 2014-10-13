5bb1fb80a12c615f7d777d91288490e436cdb753
Robert Khoury <robertkhoury1@gmail.com>
2014-10-13 14:46:07 -0700

    precompile assets

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -7,7 +7,7 @@ class HousesController < ApplicationController
   def index
     @search = House.search(params[:q])
     @search.sorts = ['zip asc', 'address desc'] if @search.sorts.empty?
-    @houses = @search.result.paginate(page: params[:page])  # load all matching records
+    @houses = @search.result.paginate(page: params[:page], per_page: 20)  # load all matching records
   end
 
   def show
